fd100eec0b258868299886911cda6885146ae151
Shimakuro <mineminou122@gmail.com>
2023-01-18 14:27:11 +0100

    Système d'authentification ajouté

--- wams/db.py
+++ wams/db.py
@@ -1,5 +1,6 @@
-from wams import db
-
+from wams import db, login_manager
+from wams import bcrypt
+from flask_login import UserMixin   
 
 class question(db.Model): 
     id = db.Column(db.Integer(), primary_key=True)
@@ -13,3 +14,24 @@ class question(db.Model):
 
     def __repr__(self):
         return f'Question {self.Label}'
+
+@login_manager.user_loader
+def load_user(id):
+    return user_info.query.get(int(id))
+
+class user_info(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    login_user = db.Column(db.String(100), nullable=False)
+    mail_user = db.Column(db.String(100), nullable=False)
+    password_user = db.Column(db.String(100), nullable=False)
+
+    @property
+    def password(self):
+        return self.password
+    
+    @password.setter
+    def password(self, plain_text_password):
+        self.password_user = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+    
+    def check_password_correction(self, passed_password):
+        return bcrypt.check_password_hash(self.password_user, passed_password)
